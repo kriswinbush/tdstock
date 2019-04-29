01dfbf35d2d878ae26d67598352b6fe7bd2cb85c
Kris Winbush <khz@kriss-mbp.lan>
2019-04-29 16:17:17 -0700

    Stock Analysts report form layout and rwd

--- src/app/analyst/analyst.module.ts
+++ src/app/analyst/analyst.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AnalystRoutingModule } from './analyst-routing.module';
 
 import { MaterialModule } from "../material/material.module";
 import { CovalentModule } from "../covalent/covalent.module";
 import { RootAnalystComponent } from './root-analyst/root-analyst.component';
+import { CovalentFileModule } from '@covalent/core/file';
 
 @NgModule({
-  declarations: [RootAnalystComponent],
+  declarations: [ RootAnalystComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CovalentFileModule,
     MaterialModule,
     CovalentModule,
     AnalystRoutingModule
--- src/app/analyst/root-analyst/root-analyst.component.ts
+++ src/app/analyst/root-analyst/root-analyst.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StepState } from "@covalent/core/steps";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-root-analyst',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RootAnalystComponent implements OnInit {
 
-  constructor() { }
+  stepForm = this.fb.group({
+    symbol: [''],
+    title: [''],
+    summary: [''],
+    storyFiles: ['']
+  });
+
+  constructor(public fb: FormBuilder) { }
+
+  onSubmit() {
+    console.log(this.stepForm.value);
+  }
+
+  activeDeactiveStep1Msg: string = 'No select/deselect detected yet';
+  stateStep2: StepState = StepState.Required;
+  stateStep3: StepState = StepState.Complete;
+  disabled: boolean = false;
+
+  toggleRequiredStep2(): void {
+    this.stateStep2 = (this.stateStep2 === StepState.Required ? StepState.None : StepState.Required);
+  }
+
+  toggleCompleteStep3(): void {
+    this.stateStep3 = (this.stateStep3 === StepState.Complete ? StepState.None : StepState.Complete);
+  }
+
+  activeStep1Event(): void {
+    this.activeDeactiveStep1Msg = 'Active event emitted.';
+  }
+
+  deactiveStep1Event(): void {
+    this.activeDeactiveStep1Msg = 'Deactive event emitted.';
+  }
 
   ngOnInit() {
+  }
+
+  submitStockStory(form) {
+    console.log("submitted")
+    console.log(form);
   }
 
 }
